e8d037e963ece5266317b1fa6c9bba12235cf251
aemery <agemery2@gmail.com>
2016-02-18 11:55:23 -0500

    First changes

--- Server.java
+++ Server.java
@@ -1,4 +1,23 @@
 public class Server {
 	
-	public 
+	public void listenSocket() {
+		try {
+			server = new ServerSocket(4321);
+		} catch (IOException e) {
+			System.out.println("Could not listen on port 4321");
+			System.exit(-1);
+		}
+		
+		try {
+			client = server.accept(); 
+		} catch (IOException e) {
+			System.out.println("Accept failed: 4321");
+			System.exit(-1);
+		}
+		
+		try {
+			in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+			out = new PrintWriter(client.getOutputStream(), true);
+		}
+	}
 }
