afd4cd32c6a19afe68371e858a17e305957bd7ca
Alexander <Alexander@Boss>
2016-02-18 14:46:46 -0500

    Add inner loop for persistent connection

--- Client.java
+++ Client.java
@@ -1,7 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 import javax.swing.JOptionPane;
 
@@ -12,14 +14,26 @@ public class Client {
 	public static int port = 7826;
 	
     public static void main(String[] args) throws IOException {
-        String serverAddress = JOptionPane.showInputDialog(
-            "Enter IP Address of a machine that is\n" +
-            "running the date service on port " + port + ":");
-        Socket s = new Socket(serverAddress, port);
-        BufferedReader input =
-            new BufferedReader(new InputStreamReader(s.getInputStream()));
-        String answer = input.readLine();
-        JOptionPane.showMessageDialog(null, answer);
+    	Scanner scanner = new Scanner(System.in);
+    	
+    	System.out.println("Enter server IP address: ");
+        String serverIP = scanner.nextLine();
+        
+        Socket socket = new Socket(serverIP, port);
+        BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        
+        while(true) {
+        	String response = input.readLine();
+        	System.out.println(response);
+        	
+        	PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        	String clientInput = scanner.nextLine();
+        	out.println(clientInput);
+        	
+        	if (clientInput.equals("Bye"))
+        		break;
+        }
+        
         System.exit(0);
     }
 }
--- Server.java
+++ Server.java
@@ -1,24 +1,42 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Date;
 
 public class Server {
 
 	public static int port = 7826;
 	
+	
     public static void main(String[] args) throws IOException {
+    	System.out.println("Stared server.");
+    	
+    	boolean isNotTerminated = true;
+    	
         ServerSocket listener = new ServerSocket(port);
         try {
-            while (true) {
+            while (isNotTerminated) {
                 Socket socket = listener.accept();
                 try {
-                    PrintWriter out =
-                        new PrintWriter(socket.getOutputStream(), true);
+                	boolean isConnected = true;
+                    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
                     out.println("Hello!");
-                } finally {
-                    socket.close();
+                    
+                    BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    
+                    	while(isConnected) {
+                    		String answer = input.readLine();
+                    		System.out.println(answer);
+                    	}
+                    		
+                } catch (IOException e) {
+                	System.out.println("Accept failed.");
+                	System.out.println(e);
+                }
+                finally {
+                	socket.close();
                 }
             }
         }
